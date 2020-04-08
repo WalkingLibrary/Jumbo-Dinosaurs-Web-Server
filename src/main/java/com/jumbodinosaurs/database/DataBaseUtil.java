20f20852a20f12cda47d26618b05043ec758ce62
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-04-08 04:20:20 -0600

    Adjustments - database addition continued
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/database/DataBaseUtil.java
+++ src/main/java/com/jumbodinosaurs/database/DataBaseUtil.java
@@ -1,9 +1,15 @@
 package com.jumbodinosaurs.database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataBaseUtil
 {
-    public static void queryDataBase(Query query, DataBase dataBase)
+    public static void queryDataBase(Query query, DataBase dataBase) throws SQLException
     {
-    
+        Connection dataBaseConnection = dataBase.getConnection();
+        Statement statement = dataBaseConnection.createStatement();
+        query.setResultSet(statement.executeQuery(query.getQuery()));
     }
 }
--- src/main/java/com/jumbodinosaurs/database/Query.java
+++ src/main/java/com/jumbodinosaurs/database/Query.java
@@ -1,5 +1,34 @@
 package com.jumbodinosaurs.database;
 
+import java.sql.ResultSet;
+
 public class Query
 {
+    private String query;
+    private ResultSet resultSet;
+    
+    public Query(String query)
+    {
+        this.query = query;
+    }
+    
+    public String getQuery()
+    {
+        return query;
+    }
+    
+    public void setQuery(String query)
+    {
+        this.query = query;
+    }
+    
+    public ResultSet getResultSet()
+    {
+        return resultSet;
+    }
+    
+    public void setResultSet(ResultSet resultSet)
+    {
+        this.resultSet = resultSet;
+    }
 }
