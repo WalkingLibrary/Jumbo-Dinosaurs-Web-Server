083e923037a6c544d82b5e45dd3eb50899d39e46
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-04-11 22:46:09 -0600

    Adjustments - database addition continued
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/database/DataBaseUtil.java
+++ src/main/java/com/jumbodinosaurs/database/DataBaseUtil.java
@@ -1,16 +1,47 @@
 package com.jumbodinosaurs.database;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+import com.jumbodinosaurs.database.exceptions.WrongStorageFormatException;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DataBaseUtil
 {
+    public static final String objectColumnName = "objectJson";
     
     public static void queryDataBase(Query query, DataBase dataBase) throws SQLException
     {
         Connection dataBaseConnection = dataBase.getConnection();
         Statement statement = dataBaseConnection.createStatement();
         query.setResultSet(statement.executeQuery(query.getQuery()));
+    }
+    
+    public static <E> ArrayList<E> getObjectsDataBase(Query query,
+                                                      DataBase dataBase,
+                                                      TypeToken<E> typeToken) throws SQLException,
+                                                                                             WrongStorageFormatException
+    {
+        queryDataBase(query, dataBase);
+        ArrayList<E> objects = new ArrayList<E>();
+        ResultSet queryResult = query.getResultSet();
+        while(queryResult.next())
+        {
+            try
+            {
+                String objectJson = queryResult.getString(objectColumnName);
+                objects.add(new Gson().fromJson(objectJson, typeToken.getType()));
+            }
+            catch(SQLException | JsonParseException e)
+            {
+                throw new WrongStorageFormatException(e.getMessage());
+            }
+        }
+        return objects;
     }
 }
