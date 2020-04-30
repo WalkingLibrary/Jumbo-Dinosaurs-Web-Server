2b5313843c5ed9afe17f1c62907ea6268cb66e05
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-04-30 09:37:17 -0600

    auth system implementation continued - reworked CheckUsername Command
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/post/CheckUsername.java
+++ src/main/java/com/jumbodinosaurs/post/CheckUsername.java
@@ -5,6 +5,7 @@ import com.jumbodinosaurs.auth.exceptions.NoSuchUserException;
 import com.jumbodinosaurs.auth.server.User;
 import com.jumbodinosaurs.auth.util.AuthSession;
 import com.jumbodinosaurs.auth.util.AuthUtil;
+import com.jumbodinosaurs.auth.util.FailureReasons;
 import com.jumbodinosaurs.devlib.database.DataBase;
 import com.jumbodinosaurs.devlib.database.exceptions.NoSuchDataBaseException;
 import com.jumbodinosaurs.devlib.database.exceptions.WrongStorageFormatException;
@@ -19,18 +20,47 @@ public class CheckUsername extends PostCommand
     public HTTPResponse getResponse(PostRequest request, AuthSession authSession)
     {
         /* Process for Checking username availability
-         * Check the given Auth Sessions user variable
-         * return 200 okay with boolean
+         *
+         * Check to see if the AuthSession's user is null
+         *
+         * Check the AuthSession's Failure Reason
+         *
+         *
          *  */
         
         HTTPResponse response = new HTTPResponse();
+    
+        //Check to see if the AuthSession's user is null
+        if(authSession.getUser() != null)
+        {
+            //Return True
+            response.setMessage200();
+            JsonObject reason = new JsonObject();
+            reason.addProperty("isUserNameTaken", true);
+            response.addPayload(reason.toString());
+            return response;
+        }
         
-        //Check the given Auth Sessions user variable
-        boolean isUserNameTaken = authSession.getUser() != null;
         
+        //Check the AuthSession's Failure Reason
+        
+        if(authSession.getFailureCode().equals(FailureReasons.MISSING_USERNAME) ||
+                   authSession.getFailureCode().equals(FailureReasons.INVALID_USERNAME))
+        {
+            response.setMessage400();
+            return response;
+        }
+        
+        if(authSession.getFailureCode().equals(FailureReasons.SERVER_ERROR))
+        {
+            response.setMessage500();
+            return response;
+        }
+        
+        //Return false
         response.setMessage200();
         JsonObject reason = new JsonObject();
-        reason.addProperty("isUserNameTaken", isUserNameTaken);
+        reason.addProperty("isUserNameTaken", false);
         response.addPayload(reason.toString());
         return response;
     }
