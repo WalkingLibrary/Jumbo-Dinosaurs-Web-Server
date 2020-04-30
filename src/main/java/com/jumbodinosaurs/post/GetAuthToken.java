49c88df716fb6b5db40224c7654343c47c8c5542
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-04-30 09:09:09 -0600

    auth system implementation continued - reworked GetAuthToken Command

--- src/main/java/com/jumbodinosaurs/post/GetAuthToken.java
+++ src/main/java/com/jumbodinosaurs/post/GetAuthToken.java
@@ -19,37 +19,62 @@ public class GetAuthToken extends PostCommand
     @Override
     public HTTPResponse getResponse(PostRequest request, AuthSession authSession)
     {
+        /* Process for Getting an auth Token
+         *
+         * Check Auth Success
+         * Get the User From the AuthSession
+         * Create a New AuthToken
+         * Set the Users AuthToken and Update the User in the DataBase
+         * Send the AuthToken in response
+         *
+         */
+        
+        
         HTTPResponse response = new HTTPResponse();
         
+        //Check Auth Success
         if(!authSession.isSuccess())
         {
             response.setMessage403();
             return response;
         }
         
+        
+        // Get the User From the AuthSession
+        User currentUser = authSession.getUser();
+        
+        
+        //Create a New AuthToken
+        String token = AuthUtil.generateRandomString(100);
+        LocalDateTime now = LocalDateTime.now();
+        AuthToken authToken;
         try
         {
-            User currentUser = authSession.getUser();
-            
-            String token = AuthUtil.generateRandomString(100);
-            LocalDateTime now = LocalDateTime.now();
-            AuthToken authToken = new AuthToken(AuthUtil.authUseName, this.ip, token, now.plusDays(30));
-            currentUser.setToken(authToken);
-            if(AuthUtil.updateUser(authSession, currentUser))
-            {
-                JsonObject object = new JsonObject();
-                object.addProperty("token", token);
-                response.setMessage200(object.toString());
-                return response;
-            }
-            response.setMessage500();
-            return response;
+            authToken = new AuthToken(AuthUtil.authUseName, this.ip, token, now.plusDays(30));
         }
         catch(PasswordStorage.CannotPerformOperationException e)
         {
             response.setMessage500();
             return response;
         }
+        
+        
+        //Set the Users AuthToken and Update the User in the DataBase
+        currentUser.setToken(authToken);
+        
+        if(AuthUtil.updateUser(authSession, currentUser))
+        {
+            JsonObject object = new JsonObject();
+            object.addProperty("token", token);
+            object.addProperty("tokenUse", AuthUtil.authUseName);
+            response.setMessage200(object.toString());
+            return response;
+        }
+        
+        response.setMessage500();
+        return response;
+        
+        
     }
     
     @Override
