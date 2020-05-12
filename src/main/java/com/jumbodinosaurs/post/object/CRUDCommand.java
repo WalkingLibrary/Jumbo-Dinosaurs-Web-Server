9e883200cf259b8d742f0560684bcb64623fd551
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-05-12 12:22:36 -0600

    Adjustments design on object posting system continued   -Added CRUDRequest Short Cutting
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/post/object/CRUDCommand.java
+++ src/main/java/com/jumbodinosaurs/post/object/CRUDCommand.java
@@ -1,36 +1,125 @@
 package com.jumbodinosaurs.post.object;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.jumbodinosaurs.auth.util.AuthSession;
+import com.jumbodinosaurs.devlib.database.exceptions.NoSuchDataBaseException;
+import com.jumbodinosaurs.devlib.database.exceptions.WrongStorageFormatException;
 import com.jumbodinosaurs.devlib.util.objects.PostRequest;
 import com.jumbodinosaurs.netty.handler.http.util.HTTPResponse;
 import com.jumbodinosaurs.post.PostCommand;
+import com.jumbodinosaurs.post.object.commands.tableCRUD.CreateTable;
+import com.jumbodinosaurs.post.object.exceptions.NoSuchTableException;
+
+import java.sql.SQLException;
 
 public abstract class CRUDCommand extends PostCommand
 {
     
-    public abstract HTTPResponse getResponse(PostRequest postRequest, AuthSession authSession, CRUDRequest crudRequest);
+    public abstract HTTPResponse getResponse(PostRequest postRequest,
+                                             AuthSession authSession,
+                                             CRUDRequest crudRequest,
+                                             Table table);
     
     @Override
     public HTTPResponse getResponse(PostRequest request, AuthSession authSession)
     {
-        return null;
-    }
-    
-    @Override
-    public boolean requiresSuccessfulAuth()
-    {
-        return false;
-    }
-    
-    @Override
-    public boolean requiresPasswordAuth()
-    {
-        return false;
-    }
-    
-    @Override
-    public boolean requiresUser()
-    {
-        return false;
+        /* Process for Short Cutting a CRUD Request
+         *
+         * Check/Verify PostRequest Attributes
+         * Check/Verify CRUDRequest Attributes
+         * Validate Table Name
+         * Filter By Create Command
+         * Check Table Permissions with AuthSession
+         *
+         *  */
+        
+        HTTPResponse response = new HTTPResponse();
+        
+        //Check/Verify PostRequest Attributes
+        if(request.getContent() == null)
+        {
+            response.setMessage400();
+            return response;
+        }
+        
+        
+        //Check/Verify CRUDRequest Attributes
+        String content = request.getContent();
+        
+        CRUDRequest crudRequest;
+        try
+        {
+            crudRequest = new Gson().fromJson(content, CRUDRequest.class);
+        }
+        catch(Exception e)
+        {
+            response.setMessage400();
+            return response;
+        }
+        
+        //Check/Verify ContentObject Attributes
+        //All CRUD request require a tableName
+        if(crudRequest.getTableName() == null)
+        {
+            response.setMessage400();
+            return response;
+        }
+        
+        //Validate Table Name
+        String tableName = crudRequest.getTableName();
+        
+        if(CRUDUtil.isValidTableName(tableName))
+        {
+            response.setMessage400();
+            return response;
+        }
+        
+        //Filter By Create Command
+        // All Commands require a table except for creating a table
+        if(request.getCommand().equals(CreateTable.class.getSimpleName()))
+        {
+            return getResponse(request, authSession, crudRequest, null);
+        }
+        
+        
+        Table table;
+        
+        try
+        {
+            table = CRUDUtil.getTable(tableName);
+        }
+        catch(NoSuchDataBaseException e)
+        {
+            response.setMessage501();
+            return response;
+        }
+        catch(SQLException | WrongStorageFormatException e)
+        {
+            response.setMessage500();
+            return response;
+        }
+        catch(NoSuchTableException e)
+        {
+            response.setMessage400();
+            JsonObject object = new JsonObject();
+            object.addProperty("failureReason", "Table Name Taken");
+            response.addPayload(object.toString());
+            return response;
+        }
+        
+        
+        //Check Table Permissions with AuthSession
+        if(!table.isPublic() && authSession.getUser() != null)
+        {
+            if(table.getPermissions(authSession.getUser().getUsername()) == null)
+            {
+                response.setMessage403();
+                return response;
+            }
+        }
+        
+        return getResponse(request, authSession, crudRequest, table);
+        
     }
 }
