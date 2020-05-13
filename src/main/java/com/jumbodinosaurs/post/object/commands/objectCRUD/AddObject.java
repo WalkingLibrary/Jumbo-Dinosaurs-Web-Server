568b07e51eddd0ecc30b4fa0bf2333edfe12b124
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-05-13 13:11:32 -0600

    Adjustments design on object posting system continued   - updated AddObject to CRUDCommand
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/post/object/commands/objectCRUD/AddObject.java
+++ src/main/java/com/jumbodinosaurs/post/object/commands/objectCRUD/AddObject.java
@@ -1,37 +1,33 @@
 package com.jumbodinosaurs.post.object.commands.objectCRUD;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
 import com.jumbodinosaurs.auth.util.AuthSession;
-import com.jumbodinosaurs.auth.util.AuthUtil;
-import com.jumbodinosaurs.devlib.database.DataBase;
-import com.jumbodinosaurs.devlib.database.DataBaseManager;
 import com.jumbodinosaurs.devlib.database.DataBaseUtil;
 import com.jumbodinosaurs.devlib.database.Query;
 import com.jumbodinosaurs.devlib.database.exceptions.NoSuchDataBaseException;
-import com.jumbodinosaurs.devlib.database.exceptions.WrongStorageFormatException;
 import com.jumbodinosaurs.devlib.util.objects.PostRequest;
 import com.jumbodinosaurs.netty.handler.http.util.HTTPResponse;
-import com.jumbodinosaurs.post.PostCommand;
 import com.jumbodinosaurs.post.object.*;
-import com.jumbodinosaurs.post.object.exceptions.NoSuchTableException;
-import com.jumbodinosaurs.util.OptionUtil;
 
 import java.sql.SQLException;
 
-public class AddObject extends PostCommand
+public class AddObject extends CRUDCommand
 {
+    
     @Override
-    public HTTPResponse getResponse(PostRequest request, AuthSession authSession)
+    public HTTPResponse getResponse(PostRequest postRequest,
+                                    AuthSession authSession,
+                                    CRUDRequest crudRequest,
+                                    Table table)
     {
         /* Process for Adding an object to a Table
          *
          *
-         * Check/Verify PostRequest Attributes
-         * Ensure it was a AuthToken Auth or Password Auth
-         * Check/Verify ContentObject Attributes
-         * Get The Table from the DataBase
+         *
+         * Check/Verify CRUDRequest Attributes
          * Validate Users Permissions on the Table
+         * Parse Object
          * Validate the Object given
          * Add the object to DataBase
          *
@@ -39,85 +35,40 @@ public class AddObject extends PostCommand
         
         HTTPResponse response = new HTTPResponse();
         
-        //Check/Verify PostRequest Attributes
-        if(request.getContent() == null)
+        
+        //Check/Verify CRUDRequest Attributes
+        if(crudRequest.getObject() == null)
         {
             response.setMessage400();
             return response;
         }
         
-        //Ensure it was a AuthToken Auth or Password Auth
-        if(!authSession.isPasswordAuth() && !authSession.getTokenUsed().getUse().equals(AuthUtil.authUseName))
+        
+        //Validate Users Permissions on the Table
+        
+        Permission permissions = table.getPermissions(authSession.getUser().getUsername());
+        if(!permissions.canAdd())
         {
             response.setMessage403();
             return response;
         }
         
         
-        //ContentObject
-        String content = request.getContent();
-    
-        CRUDRequest CRUDRequest;
-        try
-        {
-            CRUDRequest = new Gson().fromJson(content, CRUDRequest.class);
-        }
-        catch(JsonParseException e)
-        {
-            response.setMessage400();
-            return response;
-        }
-        
-        //Check/Verify ContentObject Attributes
-        if(CRUDRequest.getTableName() == null || CRUDRequest.getObject() == null)
-        {
-            response.setMessage400();
-            return response;
-        }
-        
-        //Get The Table from the DataBase
-        Table table;
-        try
-        {
-            table = CRUDUtil.getTable(CRUDRequest.getTableName());
-        }
-        catch(NoSuchTableException e)
-        {
-            response.setMessage400();
-            return response;
-        }
-        catch(WrongStorageFormatException | SQLException | NoSuchDataBaseException e)
-        {
-            response.setMessage500();
-            return response;
-        }
-        
-        
-        //Validate Users Permissions on the Table
-        if(!table.isPublic())
-        {
-            Permission permissions = table.getPermissions(authSession.getUser().getUsername());
-            if(!permissions.canAdd())
-            {
-                response.setMessage403();
-                return response;
-            }
-        }
-        
-        //Validate the Object given
-        String objectJson = CRUDRequest.getObject();
+        //Parse Object
+        String objectJson = crudRequest.getObject();
         
         PostObject postObject;
         try
         {
             postObject = new Gson().fromJson(objectJson, table.getObjectType().getType());
         }
-        catch(JsonParseException e)
+        catch(JsonSyntaxException e)
         {
             response.setMessage400();
             return response;
         }
         
+        //Validate the Object given
         if(!postObject.isValidObject())
         {
             response.setMessage400();
@@ -127,20 +78,15 @@ public class AddObject extends PostCommand
         
         //Add the object to the DataBase
         Query insertQuery = DataBaseUtil.getInsertQuery(table.getName(), postObject);
-        DataBase objectDataBase;
+        
         try
         {
-            objectDataBase = DataBaseManager.getDataBase(OptionUtil.getServersDataBaseName());
+            CRUDUtil.manipulateTableDataBase(insertQuery);
         }
         catch(NoSuchDataBaseException e)
         {
             response.setMessage501();
             return response;
-        }
-        
-        try
-        {
-            DataBaseUtil.manipulateDataBase(insertQuery, objectDataBase);
         }
         catch(SQLException e)
         {
@@ -179,5 +125,12 @@ public class AddObject extends PostCommand
     public boolean requiresUser()
     {
         return true;
+    }
+    
+    
+    @Override
+    public boolean requiresTable()
+    {
+        return false;
     }
 }
