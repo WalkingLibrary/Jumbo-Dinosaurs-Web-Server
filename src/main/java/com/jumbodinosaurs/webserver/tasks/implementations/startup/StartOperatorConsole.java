0dc095017ed4c96595e07ce9ea70a96c0fb20e2a
Caleb <43940765+WalkingLibrary@users.noreply.github.com>
2024-09-28 13:39:45 -0600

    Service Run Mode
    Signed-off-by: Caleb <43940765+WalkingLibrary@users.noreply.github.com>

--- src/main/java/com/jumbodinosaurs/webserver/tasks/implementations/startup/StartOperatorConsole.java
+++ src/main/java/com/jumbodinosaurs/webserver/tasks/implementations/startup/StartOperatorConsole.java
@@ -1,20 +1,28 @@
 package com.jumbodinosaurs.webserver.tasks.implementations.startup;
 
 
+import com.jumbodinosaurs.devlib.log.LogManager;
 import com.jumbodinosaurs.devlib.task.StartUpTask;
 import com.jumbodinosaurs.devlib.util.OperatorConsole;
 
 public class StartOperatorConsole extends StartUpTask
 {
-    
+
+    public static boolean serviceMode = true;
+
     public StartOperatorConsole()
     {
         super(0);
     }
-    
+
     @Override
     public void run()
     {
+        if (serviceMode)
+        {
+            LogManager.consoleLogger.warn("Skipping Loading Operator Console In Service Mode");
+            return;
+        }
         Thread commandThread = new Thread(new OperatorConsole());
         commandThread.start();
     }
